7482cabbf28d5e4b93d41980677364e1208d989e
MashaAtos <mariam.a.sargsyan@gmail.com>
2017-12-11 21:05:06 +0400

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,25 @@
+public class LoginTest {
+    private ChromeDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void login(){
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("#login button")).click();
+        assertTrue("Login failed",
+                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
